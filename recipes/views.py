c43731c530f6dfc9163d4e769288f89531cf448d
Victor Galvão - Analytics <victorgalvao.bsi@gmail.com>
2022-06-17 14:54:13 -0300

    Remoção Contato/Sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,6 @@
 
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
     return render(request, 'recipes/home.html',)
-
-
-def contato(request):
-    return render(request, 'temp/temp.html')
-    '''return HttpResponse('CONTATO')'''
-
-
-def sobre(request):
-    return HttpResponse('SOBRE')
